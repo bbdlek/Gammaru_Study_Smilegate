a237aa08971216f9883e5892bd5a0b4e90156dd8
bbdlek <bbdlek@gmail.com>
2021-08-17 16:50:42 +0900

    ~Week6(ppt)

---
+++ Week5/AppleCatch/Assets/Scripts/BasketController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketController : MonoBehaviour
+{
+    public AudioClip appleSE;
+    public AudioClip bombSE;
+    AudioSource aud;
+    GameObject director;
+
+    private void Awake()
+    {
+        this.director = GameObject.Find("GameDirector");
+        this.aud = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Debug.DrawRay(ray.origin, ray.direction * 10f, Color.red, 5f);
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                float x = Mathf.RoundToInt(hit.point.x);
+                float z = Mathf.RoundToInt(hit.point.z);
+                transform.position = new Vector3(x, 0.0f, z);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Apple")
+        {
+            this.director.GetComponent<GameDirector>().GetApple();
+            this.aud.PlayOneShot(this.appleSE);
+        }
+        else if(other.gameObject.tag == "Bomb")
+        {
+            this.director.GetComponent<GameDirector>().GetBomb();
+            this.aud.PlayOneShot(this.bombSE);
+        }
+        Destroy(other.gameObject);
+    }
+
+}
